bd5a4e467b3c0bb8ef12b782f7be5f62198df0fa
TaBot358 <rararan99@gmail.com>
2025-08-09 15:32:29 +0900

    🎉 Line Botの基本機能を実装

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const dotenv = require('dotenv');
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 3000;
+
+const config = {
+    channelSecret: 'CHANNEL_SECRET',
+    channelAccessToken: 'CHANNEL_ACCESS_TOKEN'
+};
+
+dotenv.config();
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
+app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+      });
+});
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+    console.log(req.body.event);
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text //実際に返信の言葉を入れる箇所
+  });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
